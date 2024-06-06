29c82181ab9ce285b944523d4517fc1dbfd2f019
3loomin <3loomin@gmail.com>
2024-06-06 21:02:08 +0900

    :sparkles: 덩치 문제 해결!

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,82 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 
 public class Main {
-    public static int zero_count = 0;
-    public static int one_count = 0;
+    public static class Human{
+        int weight;
+        int height;
+        int rank;
+
+        public Human(int weight, int height) {
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int getRank() {
+            return rank;
+        }
+
+        public void setRank(int rank) {
+            this.rank = rank;
+        }
+    }
     public static void main(String[] args) throws IOException {
         solution();
 
     }
 
     public static void solution() throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n  = Integer.parseInt(st.nextToken());
+        List<Human> list = new ArrayList<>();
+        List<Human> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            Human human = new Human(weight, height);
+            list.add(human);
+            answer.add(human);
+        }
 
         for (int i = 0; i < n; i++) {
-            int a  = Integer.parseInt(br.readLine());
-            fibonachi(a);
-            bw.write(zero_count + " " + one_count);
-            zero_count = 0;
-            one_count = 0;
-            bw.write("\n");
+            int rank = 1;
 
+            for (int j = 0; j < n; j++) {
+                if(list.get(i).weight < list.get(j).weight && list.get(i).height < list.get(j).height)
+                    rank++;
+
+            }
+            bw.write(rank + " ");
         }
+
 
         bw.flush();
         bw.close();
     }
 
-    public static long fibonachi(long n){
-        if (n == 0){
-            zero_count++;
-            return n;
-        }
-        else if( n == 1 ){
-            one_count++;
-            return n;
-        }
-        else return fibonachi(n - 1) + fibonachi(n - 2);
-    }
 
 }
---
+++ src/etc/Boj_7568.java
@@ -0,0 +1,78 @@
+package etc;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+static class Human{
+    int weight;
+    int height;
+    int rank;
+
+    public Human(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
+public class Boj_7568 {
+
+    // Main Class
+
+    public static void solution() throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n  = Integer.parseInt(st.nextToken());
+        List<Main.Human> list = new ArrayList<>();
+        List<Main.Human> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            Main.Human human = new Main.Human(weight, height);
+            list.add(human);
+            answer.add(human);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < n; j++) {
+                if(list.get(i).getWeight() < list.get(j).getWeight() && list.get(i).getHeight() < list.get(j).getHeight())
+                    rank++;
+
+            }
+            bw.write(rank + " ");
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+}
