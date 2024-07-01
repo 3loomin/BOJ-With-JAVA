1d4f1ac81e47178684503b165a395cb180b19c3c
3loomin <3loomin@gmail.com>
2024-07-01 18:04:36 +0900

    :pencil2: Life is full of ups and downs.

--- src/etc/Boj_5524.java
+++ src/etc/Boj_5524.java
@@ -1,10 +1,9 @@
+package etc;
+
 import java.io.*;
 
+public class Boj_5524 {
 
-public class Main {
-
-    
-    public static void main(String[] args) throws IOException {solution();}
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -17,5 +16,4 @@ public class Main {
         bw.flush();
         bw.close();
     }
-
 }
