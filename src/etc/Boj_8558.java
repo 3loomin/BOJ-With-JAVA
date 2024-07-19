54649057451a960e53519119dfaf81afd9b06d5e
3loomin <3loomin@gmail.com>
2024-07-19 23:57:43 +0900

    :tada: fri-day!

--- src/etc/Boj_8558.java
+++ src/etc/Boj_8558.java
@@ -1,10 +1,9 @@
+package etc;
+
 import java.io.*;
 
+public class Boj_8558 {
 
-public class Main {
-
-
-    public static void main(String[] args) throws IOException {solution();}
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
